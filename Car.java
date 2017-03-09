1c91559d5a275f9ec2071228b35993a08399b653
Oliver Christensen <christensenoliver27@gmail.com>
2017-03-09 13:53:33 +0100

    added a java file with cars

---
+++ Car.java
@@ -0,0 +1,16 @@
+/**
+ * Created by narto_000 on 3/9/2017.
+ */
+public class Car {
+
+    private String type;
+    private int numOfWheels;
+    private String color;
+
+    public void Car(String name, int numOfWheels, String color) {
+        this.color = color;
+        this.numOfWheels = numOfWheels;
+        this.type = type;
+    }
+
+}
